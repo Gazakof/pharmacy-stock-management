38757b09373c0cb4339f38a8420127aaf57e6f1a
Gazakof <randriamorasata08@gmail.com>
2025-03-06 08:43:28 +0100

    call API correct

--- Frontend/pages/dashboard/dashboard.js
+++ Frontend/pages/dashboard/dashboard.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", async function () {
       }
       medicines = await response.json();
       displayMedecines(medicines);
+      if (medicines.length > 0) displayMedecineDetails(0);
     } catch (error) {
       console.log(error);
     }
@@ -30,13 +31,55 @@ document.addEventListener("DOMContentLoaded", async function () {
     const tableBody = document.getElementById("medecine-list");
     tableBody.innerHTML = "";
 
-    medicines.forEach((med) => {
+    medicines.forEach((med, index) => {
       const row = document.createElement("tr");
       row.innerHTML = `<td class="medecine-name">${med.name}</td>
             <td class="medecine-quantity">${med.quantity}</td>`;
+      row.addEventListener("click", () => displayMedecineDetails(index));
       tableBody.appendChild(row);
     });
   }
+
+  function displayMedecineDetails(index) {
+    if (medicines.length === 0) return;
+
+    currentIndex = index;
+    const med = medicines[index];
+
+    const date = new Date(med.expiration);
+    const formattedDate = date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "short",
+      day: "2-digit",
+    });
+
+    document.getElementById("content-title").textContent = med.name;
+    document.getElementById("medicine-name").textContent = med.name;
+    document.getElementById("medicine-manufacturer").textContent =
+      med.manufacturer;
+    document.getElementById("medicine-price").textContent = `${med.price}€`;
+    document.getElementById(
+      "medicine-expiration"
+    ).textContent = `Expiration: ${formattedDate}`;
+    document.getElementById("medicine-number").textContent = med.quantity;
+  }
+
+  document.getElementById("prev-Btn").addEventListener("click", () => {
+    if (currentIndex == 0) {
+      displayMedecineDetails(medicines.length - 1);
+      return;
+    }
+    if (currentIndex > 0) displayMedecineDetails(--currentIndex);
+  });
+
+  document.getElementById("next-Btn").addEventListener("click", () => {
+    if (currentIndex == medicines.length - 1) {
+      displayMedecineDetails(0);
+      return;
+    }
+    if (currentIndex < medicines.length - 1)
+      displayMedecineDetails(++currentIndex);
+  });
 
   document.getElementById("onLogout").addEventListener("click", function () {
     localStorage.removeItem("token");
